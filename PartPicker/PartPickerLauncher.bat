ac39ca209618ad2b18a1301d2874d777ac49844e
kianz20 <95212993+kianz20@users.noreply.github.com>
2021-11-30 17:59:37 +1300

    Rename PartPickerLauncher.bat to PartPicker/PartPickerLauncher.bat

---
+++ PartPicker/PartPickerLauncher.bat
@@ -0,0 +1,1 @@
+java --module-path "<Your JavaFX SDK Path>" --add-modules javafx.controls,javafx.fxml,javafx.base,javafx.graphics -jar PartPicker.jar
--- PartPickerLauncher.bat
+++
@@ -1,1 +0,0 @@
-java --module-path "<Your JavaFX SDK Path>" --add-modules javafx.controls,javafx.fxml,javafx.base,javafx.graphics -jar PartPicker.jar
